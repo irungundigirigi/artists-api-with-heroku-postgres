149f23868928f6d477460fa2a2f2c140e8202d3d
Edwin Irungu <irungundigirigi@gmail.com>
2021-05-21 09:47:24 +0300

    delete, update and get artist by id routesadded

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const pool = new Pool ({
 });
 pool.connect();
 
-
+//CREATE AN ARTIST
 app.post("/api/artists",async(req,res)=> {
     try{
         const {first_name, last_name, stage_name,sex,date_of_birth, email,genre,records_sold,active,id_no} = req.body;
@@ -35,13 +35,14 @@ app.post("/api/artists",async(req,res)=> {
             "INSERT INTO artists (first_name, last_name, stage_name,sex,date_of_birth, email,genre,records_sold,active,id_no) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *",
             [first_name, last_name, stage_name,sex,date_of_birth, email,genre,records_sold,active,id_no]
         );
+        
         res.json(newArtist.rows[0]);
     }catch(err){
         console.log(err.message )
     }
 });
 
-//GET  TODOs
+//GET ARTISTS
 app.get("/api/artists",async(req,res) => {
     try{
         const allArtists = await pool.query("SELECT * FROM artists");
@@ -53,10 +54,60 @@ app.get("/api/artists",async(req,res) => {
     }
 });
 
+//GET AN ARTIST
+app.get("/api/artists/:id_no",async(req,res) => {
+    const{id_no} = req.params
+    try{
+        const Artist = await pool.query("SELECT * FROM artists WHERE id_no = $1", [id_no]);
+        res.json(Artist.rows[0]);    
+
+    }catch(err){
+        console.error(err.message);
+    }    
+
+});
+
+//UPDATE AN ARTIST
+app.put("/api/artists/:id_no",async(req,res) => {
+    const{id_no} = req.params
+    const {first_name, last_name, stage_name,sex,date_of_birth, email,genre,records_sold,active} = req.body;
+    try{
+        const updateArtist= await pool.query("UPDATE artists SET first_name= $1,last_name=$2, stage_name =$3,sex = $4,date_of_birth = $5, email = $6,genre=$7,records_sold = $8,active = $9 WHERE id_no = $10",[first_name, last_name, stage_name,sex,date_of_birth, email,genre,records_sold,active,id_no]
+        );
+        res.json("Artist was updated!");    
+
+    }catch(err){
+        console.log(err.message);
+    }    
+
+});
+
+//DELETE AN ARTIST
+app.delete('/api/artists/:id_no', async (req, res) => {
+    try{
+        const{id_no} = req.params;
+        const deleteArtist = await pool.query(
+        "DELETE FROM artists WHERE id_no = $1",
+         [id_no]
+        );
+        
+        res.json("Artist deleted successfully!");    
+    }catch(err){
+        console.log(err.message);
+    }    
+
+});
+
+
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`App is running at http://localhost:${port}`)
-} )
+})
+
 
 
 
