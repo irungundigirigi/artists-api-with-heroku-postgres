f82919fdde1e81d95ad18947bccd809105ff5a9c
Edwin Irungu <irungundigirigi@gmail.com>
2021-06-07 21:24:50 +0300

    minor changes

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const pg = require('pg');
-//const {Pool} = require('pg');
+const {Pool} = require('pg');
 //let Password = process.env.DB_PASSWORD;
 require('dotenv').config()
 
@@ -10,7 +10,10 @@ require('dotenv').config()
 //const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  connectionString:process.env.DATABASE_URL
+  connectionString:process.env.DATABASE_URL,
+  ssl: config.ENV === 'production' && {
+      rejectUnauthorized: false,
+  }
 })
 pool.connect() 
 
